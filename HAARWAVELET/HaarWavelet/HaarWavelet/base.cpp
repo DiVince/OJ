2c369aa019d6dde9b3e6126e0fb0e7b086c77d82
heaviside layer <13825889+DiVince@users.noreply.github.com>
2017-10-27 21:32:31 +0800

    base(80%)

--- HAARWAVELET/HaarWavelet/HaarWavelet/base.cpp
+++ HAARWAVELET/HaarWavelet/HaarWavelet/base.cpp
@@ -2,13 +2,39 @@
 #include<cmath>
 using namespace std;
 
-
+void sort(int parts, int length, double* p)
+{
+	double temp;
+	int m;
+	while (length > 2)
+	{
+		m = length / 2;
+		//cout << "parts = " << parts << '\t' << "length = " << length << endl;
+		for (int j = 0; j < parts; j++)
+		{
+			for (int i = 0; i < (m / 2); i++)
+			{
+				temp = p[2 * i + 1 + j * length];
+				p[2 * i + 1 + j * length] = p[2 * i + m + j * length];
+				p[2 * i + m + j * length] = temp;
+				/*
+				for (int i = 0; i < length; i++)
+				{
+					cout << p[i] << ' ';
+				}
+				cout << endl << endl;
+				*/
+			}
+		}
+		parts *= 2;
+		length = m;
+	}
+}
 
 void wavelet(int L, double* p)
 {
 	int n = pow(2, L);
 	int m = n / 2;
-	double*q = new double[m];
 	/*
 	cout << "L: " << L << endl;
 	for (int i = 0; i < n; i++)
@@ -19,15 +45,8 @@ void wavelet(int L, double* p)
 	*/
 	for (int i = 0; i < m; i++)
 	{
-		q[i] = -(p[2 * i] - p[2 * i + 1]) / 2;
-	}
-	for (int i = 0; i < m; i++)
-	{
-		p[i] = (p[2 * i] + p[2 * i + 1]) / 2;
-	}
-	for (int i = m; i < n; i++)
-	{
-		p[i] = q[i - m];
+		p[2 * i] = (p[2 * i] + p[2 * i + 1]) / 2;
+		p[2 * i + 1] = -(p[2 * i] - p[2 * i + 1]);
 	}
 	/*
 	for (int i = 0; i < n; i++)
@@ -36,9 +55,17 @@ void wavelet(int L, double* p)
 	}
 	cout << endl << endl;
 	*/
-	delete[] q;
+	sort(1, n, p);
+	/*
+	for (int i = 0; i < n; i++)
+	{
+		cout << p[i] << ' ';
+	}
+	cout << endl << endl;
+	*/
 }
 
+#if 1
 int main()
 {
 	cout.setf(ios::fixed);
@@ -48,10 +75,8 @@ int main()
 	l = L;
 	n = pow(2, L);
 	double* p = new double[n];
-	//double* q = new double[n];
 	for (int i = 0; i < n; i++)
 		cin >> p[i];
-	//cout << endl << endl;
 	while (l >= 1)
 	{
 		wavelet(l, p);
@@ -63,3 +88,27 @@ int main()
 	}
 	return 0;
 }
+#endif
+
+#if 0
+int main()
+{
+	double a[16];
+	for (int i = 0; i < 16; i++)
+	{
+		a[i] = i;
+	}
+	for (int i = 0; i < 16; i++)
+	{
+		cout << a[i] << ' ';
+	}
+	cout << endl << endl;
+	sort(1, 16, a);
+	for (int i = 0; i < 16; i++)
+	{
+		cout << a[i] << ' ';
+	}
+	cout << endl << endl;
+	return 0;
+}
+#endif
