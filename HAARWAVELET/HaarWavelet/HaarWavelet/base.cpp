8e53991591cecd77875116667cc3c48c1c7f2785
heaviside layer <13825889+DiVince@users.noreply.github.com>
2017-10-27 23:10:38 +0800

    base(100%)

--- HAARWAVELET/HaarWavelet/HaarWavelet/base.cpp
+++ HAARWAVELET/HaarWavelet/HaarWavelet/base.cpp
@@ -5,21 +5,19 @@ using namespace std;
 void sort(int parts, int length, float* p)
 {
 	float temp;
-	int m;
 	while (length > 2)
 	{
-		m = length / 2;
 		for (int j = 0; j < parts; j++)
 		{
-			for (int i = 0; i < (m / 2); i++)
+			for (int i = 0; i < (length / 4); i++)
 			{
 				temp = p[2 * i + 1 + j * length];
-				p[2 * i + 1 + j * length] = p[2 * i + m + j * length];
-				p[2 * i + m + j * length] = temp;
+				p[2 * i + 1 + j * length] = p[2 * i + length / 2 + j * length];
+				p[2 * i + length / 2 + j * length] = temp;
 			}
 		}
 		parts *= 2;
-		length = m;
+		length /= 2;
 	}
 }
 
@@ -39,17 +37,16 @@ int main()
 {
 	cout.setf(ios::fixed);
 	cout.precision(4);
-	int L, l, n;
+	int L, n;
 	cin >> L;
-	l = L;
 	n = pow(2, L);
 	float* p = new float[n];
 	for (int i = 0; i < n; i++)
 		cin >> p[i];
-	while (l >= 1)
+	while (L >= 1)
 	{
-		wavelet(l, p);
-		l--;
+		wavelet(L, p);
+		L--;
 	}
 	for (int i = 0; i < n; i++)
 	{
